ff040a3c16fe60a8817e1f5208dec1d1963e0e47
Raju994 <bangarraju994@gmail.com>
2023-11-30 18:45:25 +0530

    search input is added along with class component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import UserProfiles from "./Components/UserProfiles";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const userDetailsList = [
+    { 
+      uniqueNo: 1,
+      name: 'Virat Kohli',
+      role: 'Right-Arm Batsman',
+      age: 35,
+      centuries: 80,
+      Wickets: 300
+    },
+    { 
+      uniqueNo: 2,
+      name: 'AB Devilliers',
+      role: 'Right-Arm Batsman',
+      age: 39,
+      centuries: 50,
+      Wickets: 300
+    },
+    { 
+      uniqueNo: 3,
+      name: 'MS Dhoni',
+      role: 'Right-Arm Batsman',
+      age: 41,
+      centuries: 35,
+      Wickets: 300
+    },
+    { 
+      uniqueNo: 4,
+      name: 'Rohit Sharma',
+      role: 'Right-Arm Batsman',
+      age: 36,
+      centuries: 50,
+      Wickets: 300
+    },
+    { 
+      uniqueNo: 5,
+      name: 'Suresh Raina',
+      role: 'Left-Arm Batsman',
+      age: 38,
+      centuries: 42,
+      Wickets: 300
+    },
+    { 
+      uniqueNo: 6,
+      name: 'Jasprit Bhumra',
+      role: 'Right-Arm Fast Bowler',
+      age: 30,
+      centuries: 0,
+      Wickets: 300
+    }
+]
+
+
+class UserDetails extends Component {
+  render(){
+    return(
+  <div > 
+    <div className="app">
+      <h1> Users List </h1>
+      <input type="serch"/>
     </div>
-  );
-}
+    <div> 
+      <ul>
+        {userDetailsList.map((eachItem) => 
+        (<UserProfiles userDetails = {eachItem}/>
+        ))}
+      </ul>
+    </div>
+  </div>)}}
 
-export default App;
+
+export default UserDetails;
---
+++ src/Components/UserProfiles/index.js
@@ -0,0 +1,22 @@
+import "./index.css";
+
+const UserProfiles = (props) => {
+    const {userDetails} = props
+    const {name, role, age, centuries, Wickets} = userDetails
+
+    return(
+        <li>
+            <div className="app">
+                <h1> {name} </h1>
+                <h2> Role: {role} </h2>
+                <h2> Age: {age} </h2>
+                <h2> Centuries: {centuries} </h2>
+                <h2> Wickets: {Wickets} </h2>
+            </div>
+        </li>
+    )
+
+
+};
+
+export default UserProfiles;
